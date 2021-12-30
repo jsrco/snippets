51b84c1ccbe4e932bbd80c65331173afa20bdb16
Ryan Orlando <jsrco@hotmail.com>
2021-12-29 23:30:36 -0800

    Prep for AWS.

--- GridTraveler.js
+++ GridTraveler.js
@@ -8,3 +8,18 @@ const gridTraveler = (x, y, memo = {}) => {
     memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo)
     return memo[key]
 }
+
+
+/**
+ * make it work  
+ * visualize the prolbem as a tree
+ * implement the tree using recursion
+ * test it
+ */ 
+
+/**
+ * make it efficient,
+ * add a memo object
+ * add a base case to return memo values
+ * store return values into the memo
+ */
