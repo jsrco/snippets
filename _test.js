b37e06495f4a4983094680dadb06dc71be95366a
Ryan Orlando <jsrco@hotmail.com>
2022-01-04 15:16:36 -0800

    Update _test.js

--- _test.js
+++ _test.js
@@ -9,10 +9,23 @@
  * -how should I lable the important pieces of data that are a part of the problem
  *  
  * Explore Concrete Examples
+ * -start with simple examples
+ * -progress to more complex
+ * -explore examples with empty inputs 
+ * -explore examples with invalid inputs
  * 
  * Break It Down
+ * -explicity write down the steps you need to take
  * 
  * Solve Or Simplify
+ * -try to solve and if you can't try to solve and easier part of the problem
  * 
  * Look Back and Refactor
+ * -can you check the result
+ * -can you derive the result differently 
+ * -can you understand it at a glance
+ * -can you use the result or method for another problem
+ * -can you improve performance
+ * -can you think of other ways of writing the code
+ * -how have other people solved it
  */
