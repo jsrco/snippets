09e033e7a966f175c6ec001dac783dbdbe768272
Ryan Orlando <jsrco@hotmail.com>
2022-01-05 01:16:58 -0800

    Study
    Reviewed previous question that stumped me with best answer. Reverse as well from blog.
    
    Prepped _test file for printing.

---
+++ AreThereDuplicates.js
@@ -0,0 +1,28 @@
+const areThereDuplicates =() => {
+    let collection = {}
+    for(let val in arguments){
+      collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+    }
+    for(let key in collection){
+      if(collection[key] > 1) return true
+    }
+    return false;
+  }
+
+
+
+  const multiplePointers = (...args) => {
+    // Two pointers
+    args.sort((a,b) => a > b);
+    let start = 0;
+    let next = 1;
+    while(next < args.length){
+      if(args[start] === args[next]){
+          return true
+      }
+      start++
+      next++
+    }
+}
+
+  const oneLineVersion = () => { return new Set(arguments).size !== arguments.length; }
